768019d4b05be3b490f12011288423da50fc9a0c
bodascaluca <bodasca.luca@gmail.com>
2022-04-19 16:33:03 +0200

    Change solution snack-4

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,32 @@
-alert("ciao")
+// jsnack1. L'utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+//  L'utente inserisce due parole in successione, con due prompt.
+
+const firstWord = prompt("Dimmi una parola");
+console.log(firstWord);
+
+const secondWord = prompt("Dimmi un'altra parola")
+console.log(secondWord);
+
+// Troviamo la lunghezza delle parole
+
+const firstWordLength = firstWord.length;
+console.log(firstWordLength);
+
+const secondWordLength = secondWord.length;
+console.log(secondWordLength);
+
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+if (firstWordLength > secondWordLength){
+    result = `${secondWord} + ${firstWord}`;
+} else if (firstWord < secondWord){
+    result = `${firstWord} ${secondWord}`
+} else {
+    result =`Le due parole sono uguali`;
+}
+
+//Output 
+console.log(result);
--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -3,14 +3,28 @@
 
 // Chiedi un numero di 4 cifre all'utente
 
-let originalNumber = parseInt(prompt("Scrivi un numero di 4 cifre"));
+let originalNumber = prompt("Scrivi un numero di 4 cifre");
 console.log(originalNumber);
 
 // e calcola la somma di tutte le cifre che compongono il numero.
 sum = 0;
 
+// for (let i = 0; i < originalNumber.length; i++) {
+//     console.log(originalNumber.charAt(i));
+//     sum += originalNumber
+// }
+
+let i = 0;
+while (i < originalNumber.length){
+    sum += parseInt(originalNumber[i]);
+    i++;
+}
+console.log(sum);
+
+/*
 while (originalNumber) {
     sum += originalNumber % 10;
     originalNumber = Math.floor(originalNumber / 10);
 }
 console.log(sum);
+*/
