1ecceda4dffd20196fd04dc87e686840fcaf1e7e
bodascaluca <bodasca.luca@gmail.com>
2022-04-19 17:34:47 +0200

    Add snack-3

---
+++ snack-3/js/script.js
@@ -0,0 +1,15 @@
+// Calcola la somma e la media dei primi 10 numeri.
+let somma = 0;
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    somma = somma + i;
+}
+
+console.log(`La somma è`,somma);
+
+const media = somma / 10; 
+console.log(`Questa è la media`,media)
+
+
+
